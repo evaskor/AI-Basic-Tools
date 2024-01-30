9825894a67090c9b277a45fbb22111fb38c89792
Evelyne <evaskor7@gmail.com>
2024-01-30 08:06:30 -0800

    Basic AI codes
    Base to start deploying

---
+++ llm.py
@@ -0,0 +1,96 @@
+import openai
+import nlpcloud
+import cohere
+import json
+
+"""
+LLMs:
+OpenAI
+NlpCloud
+Cohere
+You can add your own
+"""
+
+
+def llm_generate_text_with_save(prompt, service, model):
+    if service == "OpenAI":
+        generated_text = openai_generate(prompt, model)
+        # save generated text
+    elif service == "NlpCloud":
+        generated_text = nlp_cloud_generate(prompt, model)
+    elif service == "Cohere":
+        generated_text = cohere_generate(prompt, model)
+
+    save_to_json(prompt, generated_text)
+    return generated_text
+
+
+def save_to_json(prompt, response):
+    data = {"prompt": prompt, "response": response}
+    with open("ai_prompts_responses.json", "a") as f:
+        f.write(json.dumps(data) + "\n")
+
+
+def llm_generate_text(prompt, service, model):
+    if service == "OpenAI":
+        generated_text = openai_generate(prompt, model)
+    elif service == "NlpCloud":
+        generated_text = nlp_cloud_generate(prompt, model)
+    elif service == "Cohere":
+        generated_text = cohere_generate(prompt, model)
+
+    return generated_text
+
+
+# Open AI Function
+openai.api_key = "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"
+def openai_generate(user_prompt, selected_model):
+    completion = openai.ChatCompletion.create(
+        model=selected_model, 
+        messages=[{"role": "user", "content": user_prompt}
+        ]
+    )
+    return completion.choices[0].message['content']
+
+
+# nlpCloud Function
+nlp_cloud_key = "e0f606e6059ca70d6abcac15259977771427b5ad"
+
+
+def nlp_cloud_generate(user_prompt, selected_model):
+    client = nlpcloud.Client(selected_model, nlp_cloud_key, gpu=True)
+    result = client.generation(
+        user_prompt,
+        max_length=50,
+        length_no_input=True,
+        remove_input=True,
+        end_sequence=None,
+        top_p=1,
+        temperature=0.8,
+        top_k=50,
+        repetition_penalty=1,
+        num_beams=1,
+        num_return_sequences=1,
+        bad_words=None,
+        remove_end_sequence=False
+    )
+    return result["generated_text"]
+
+
+# Cohere API
+cohere_api_key = "A6Ya68o5rolhEKnSP1TRn0p1jQ1WMrNd0pdNgzVK"
+
+
+def cohere_generate(user_prompt, selected_model):
+    co = cohere.Client(cohere_api_key)  # This is your trial API key
+    response = co.generate(
+        model=selected_model,
+        prompt=user_prompt,
+        max_tokens=300,
+        temperature=0.9,
+        k=0,
+        stop_sequences=[],
+        return_likelihoods="NONE",
+    )
+
+    return response.generations[0].text
